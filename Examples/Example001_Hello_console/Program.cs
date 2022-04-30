031c6200cf9a89e6c7817becb13abb099cc9f90a
Zerotools <prosoundmsk@mail.ru>
2022-04-30 14:13:50 +0300

    Example 1,2 & .gitignore

--- Examples/Example001_Hello_console/Program.cs
+++ Examples/Example001_Hello_console/Program.cs
@@ -5,8 +5,12 @@ namespace Example001_Hello_console
     class Program
     {
         static void Main(string[] args)
+        
         {
-            Console.WriteLine("2Hello World!");
+            Console.WriteLine("Привет. Как тебя зовут?");
+            string username = Console.ReadLine();
+            Console.Write("Привет, ");
+            Console.WriteLine(username);
         }
     }
 }
