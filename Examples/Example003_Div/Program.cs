75fe842761d7533f0b80001b09403eefa3fe5e8e
Zerotools <prosoundmsk@mail.ru>
2022-04-30 16:36:08 +0300

    Example003

---
+++ Examples/Example003_Div/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Example003_Div
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = 12;
+            double numberB = 5;
+            Console.WriteLine();
+            Console.WriteLine(numberA/numberB);
+        }
+    }
+}
