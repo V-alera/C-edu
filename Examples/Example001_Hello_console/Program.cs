fa7894d57917a9988c09c5ebc9fe31c6928288a2
Zerotools <prosoundmsk@mail.ru>
2022-04-30 13:28:42 +0300

    New languade C#

---
+++ Examples/Example001_Hello_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Example001_Hello_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2Hello World!");
+        }
+    }
+}
