2ba0dd6ed5cc7401c273dc60e69e10c1cecb41cc
Zerotools <prosoundmsk@mail.ru>
2022-04-30 17:25:40 +0300

    Example004. Random numbers

---
+++ Examples/Example004_random_number/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Example003_Div
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double numberA = new Random().Next(-100000,10000);
+            double numberB = new Random().Next(-999999,999999);
+            Console.WriteLine();
+            Console.Write("Загадано число A = ");
+            Console.Write(numberA);
+            Console.Write("   Загадано число B = ");
+            Console.WriteLine(numberB);
+            Console.Write("Деление A/B = ");
+            Console.Write(numberA/numberB);
+        }
+    }
+}
