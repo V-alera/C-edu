7017879b02e04898e19bd8e77265d78203728068
Zerotools <prosoundmsk@mail.ru>
2022-04-30 14:45:19 +0300

    Example2

---
+++ Examples/Example002_sum/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Example002_sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberA = 3;
+            int numberB = 5;
+            int result = numberA+numberB;
+            Console.WriteLine();
+            Console.WriteLine(result);
+        }
+    }
+}
