aa01d174895860077c739ae9424d41b31b5e4c6b
Zerotools <prosoundmsk@mail.ru>
2022-05-13 16:29:54 +0300

    Знакомство с языками программирования (семинары). Домашнее задание 2, задача 13

---
+++ Programming languages Task_13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Seminar_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int num = 0;
+            Console.WriteLine();
+            Console.WriteLine("Введите число");
+            Console.WriteLine();
+            
+            str = Console.ReadLine();
+            num = Convert.ToInt32(str);
+            
+            while (num > 1000 || num < -1000) num/=10;
+
+            Console.WriteLine();
+            
+            if (num < 0) Console.Write("Третья цифра числа: " + num *-1 % 10);
+            else Console.Write("Третья цифра числа: " + num % 10);
+            Console.WriteLine();
+        }
+    }
+}
