f19cf335784c66edc334748bb6d89c2db78e6f29
Zerotools <prosoundmsk@mail.ru>
2022-05-13 14:09:50 +0300

    Знакомство с языками программирования (семинары). Домашнее задание 2, задача 10

---
+++ Programming languages Task_10/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Seminar_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int num = 0, ostatok = 0;
+            Console.WriteLine();
+            Console.WriteLine("Введите число");
+            Console.WriteLine();
+            
+            str = Console.ReadLine();
+            num = Convert.ToInt32(str);
+            ostatok = num / 10 % 10;
+            
+            Console.WriteLine();
+            
+            Console.Write("Вторая цифра трехзначного числа: " + ostatok);
+             
+            Console.WriteLine();
+        }
+    }
+}
