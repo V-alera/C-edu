3d2f1dd189b3640acc3e0bcc54722ae0c613d6ac
Zerotools <prosoundmsk@mail.ru>
2022-05-23 18:38:23 +0300

    HW3_Seminar3_Task23_programming_languages

---
+++ HW3_Seminar3_Task23_programming_languages/Program.cs
@@ -0,0 +1,40 @@
+﻿/*
+Задача 23: Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125
+*/
+
+using System;
+
+namespace Seminar_Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+            int num = 0, N = 0;
+            Console.WriteLine();
+            Console.Write("Введите число ");
+                      
+            str = Console.ReadLine();
+            N = Convert.ToInt32(str);
+            int[] arr = new int[N+1];
+                for (int i = 1; i <= N; i++)
+                {
+                arr[i] = i*i*i;
+                }
+            Console.WriteLine();
+            Console.Write("Список кубов до числа " + N);    
+            Console.Write("-> ");
+               for (int i = 1; i <= N; i++)
+                {
+                Console.Write(arr[i] + " ");
+                } 
+            
+            Console.WriteLine();
+                       
+        }
+    }
+}
