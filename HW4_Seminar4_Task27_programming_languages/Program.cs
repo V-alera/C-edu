a997a61e51bda379148a78c92fea50c12b19fc05
Zerotools <prosoundmsk@mail.ru>
2022-05-26 14:27:03 +0300

    HW4_Seminar4_Task27_programming_languages

---
+++ HW4_Seminar4_Task27_programming_languages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HW4_Seminar4_Task27_programming_languages
+{
+    class Program
+    {
+        static int FillData()
+        {
+            Console.Write("Введите число: ");
+            int num = int.Parse(Console.ReadLine());
+            return num;
+        }
+        static int GetResult (int num)
+        {
+            int res = 0;
+            Console.WriteLine($"Суммируем все цифры числа {num}");
+            while (num !=0)
+            {
+                res+=num % 10;
+                num/=10;
+            }
+            return res;
+        }
+        static void PrintResult (int res)
+        {
+            Console.WriteLine ($"Сумма цифр составляет: {res}");
+        }
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int number = FillData();
+            int sum = GetResult(number);
+                     PrintResult(sum);
+        }
+    }
+}
