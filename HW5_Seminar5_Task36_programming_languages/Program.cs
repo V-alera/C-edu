3439c5d91a49852b617b9633e77e0f7dd54c7df4
Zerotools <prosoundmsk@mail.ru>
2022-05-29 22:55:53 +0300

    HW5_Seminar5_Task36_programming_languages

---
+++ HW5_Seminar5_Task36_programming_languages/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный
+случайными числами. Найдите сумму элементов, стоящих
+на нечётных позициях.
+*/
+
+using System;
+
+namespace HW5_Seminar5_Task34_programming_languages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            int sumOddElement = 0;
+            Console.Write("Введите количество элементов массива: ");
+                int size = int.Parse(Console.ReadLine());
+                    int[] mass = new int[size];
+
+                    var newRnd = new Random();
+            Console.WriteLine();
+            for (int i = 0; i < size; i++)
+            {
+                mass[i] = newRnd.Next(-100, 100);
+                Console.Write(mass[i] + " ");
+                if (i % 2 != 0) sumOddElement+=mass[i];
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.Write($"Сумма элементов на нечетных местах равна {sumOddElement} ");
+            Console.WriteLine();
+        }
+    }
+}
